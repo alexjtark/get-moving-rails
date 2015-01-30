c7b682416e4997bb635faff167fd589812bbc415
Alex Tarkowski <alex.tarkowski@gmail.com>
2015-01-30 17:21:16 -0500

    add password validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
   validates :username, length: { minimum: 4 }
   validates :email, uniqueness: true
   validates :email, presence: true
+  validates :password, length: { minimum: 6 }
   has_many :boxes
   has_many :items, through: :boxes
   has_secure_password
+
 end
