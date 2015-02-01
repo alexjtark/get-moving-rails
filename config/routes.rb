464d41423c84f271c51f8058b4980feedb8f64a8
alexjtark <alex.tarkowski@gmail.com>
2015-02-01 18:26:18 -0500

    add pages controller and main view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
+  resources :boxes
+  resources :items
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root :to => 'pages#main'
 end
